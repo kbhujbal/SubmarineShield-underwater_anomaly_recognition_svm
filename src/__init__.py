65cdab9bd3f54d8273e4a0a272c5bee55afac105
kbhujbal <kbhujbal@usc.edu>
2023-08-16 12:00:00 -0700

    init

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""
+Submarine AI - Sonar Rock vs Mine Classification
+A production-grade ML module for underwater threat detection using sonar signals.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Submarine AI Team"
